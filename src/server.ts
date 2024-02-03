ecb0d4eb8825c80f76f5835d50d595e84cf02d6d
Alessandro0003 <alessandroquinta3@gmail.com>
2024-02-03 00:45:18 -0300

    :sparkles: feat: success conection fastify and settings environment variables

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import { env } from './env'
+import { app } from './app'
+
+
+app.listen({
+    host: '0.0.0.0',
+    port: env.PORT
+}).then(() => {
+    console.log(`🚀 HTTP Server RuNning in http://localhost:${env.PORT}`)
+})

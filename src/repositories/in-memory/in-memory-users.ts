d7d5480369e14c6a4b7dd887f8939752936e578a
Alessandro0003 <alessandroquinta3@gmail.com>
2024-02-03 03:52:02 -0300

    :test_tube: test: create tests to creating users and to create hash a password

--- src/repositories/in-memory/in-memory-users.ts
+++ src/repositories/in-memory/in-memory-users.ts
@@ -1,11 +1,34 @@
 import { Prisma, User } from '@prisma/client'
 import { UsersRepository } from '../users/users-repository'
 import { randomUUID } from 'node:crypto'
+    
 
-export class InMemoryUsersRepository implements UsersRepository {   
-    public items: User[] = []
+export class InMemoryUsersRepository implements UsersRepository { 
 
-    async create(data: Prisma.UserCreateInput) {
+   public items: User[] = []
+
+   async  findById(id: string) {
+      const user = this.items.find((item) => item.id === id)
+
+      if (!user) {
+        return null
+      }
+
+      return user
+   }
+
+
+   async  findByEmail(email: string) {
+      const user = this.items.find((item) => item.email === email)
+
+      if (!user) {
+        return null
+      }
+
+      return user
+   }
+
+   async create(data: Prisma.UserCreateInput) {
         const user = {
             id: randomUUID(),
             name: data.name,
@@ -20,7 +43,7 @@ export class InMemoryUsersRepository implements UsersRepository {
         this.items.push(user)
 
         return user
-    }
+   }
 
-
+   
 }
---
+++ tests/use-case/users/create-users.spec.ts
@@ -0,0 +1,51 @@
+import { expect, it, describe, beforeEach } from 'vitest'
+import { compare } from 'bcryptjs'
+import { CreateUsersUseCases } from '@/use-case/users/create-users'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users'
+
+let usersRepository: InMemoryUsersRepository
+let sut: CreateUsersUseCases
+
+describe('Register Use Case', () => {
+
+    beforeEach(() => {
+        usersRepository = new InMemoryUsersRepository()
+        sut = new CreateUsersUseCases(usersRepository)
+    })
+
+    it('should be able to register', async () => {
+        
+        const { user } =  await sut.execute({
+            name: 'Alessandro',
+            email: 'ale@example.com',
+            password: '123456',
+            address: 'Curitiba',
+            phone: '(41)99149-5889',
+            isAdmin: 'MEMBER'
+        })
+
+        expect(user.id).toEqual(expect.any(String))
+    })
+
+
+    it('should hash user password upon registration', async () => {
+
+        const { user } =  await sut.execute({
+            name: 'Alessandro',
+            email: 'ale@example.com',
+            password: '123456',
+            address: 'Curitiba',
+            phone: '(41)99149-5889',
+            isAdmin: 'MEMBER'
+        })
+
+        const isPasswordCorrectlyHashed = await compare(
+            '123456',
+            user.password_hash,
+        )
+            
+        expect(isPasswordCorrectlyHashed).toBe(true)
+
+    })
+
+})
